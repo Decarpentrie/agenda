8f892d43363fa9984c559c14cd04c5dc7fbb96a9
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-03-30 14:40:48 +0200

    affichage des objet dans la console

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var agenda=[];
+
+$("#save").click(function() {
+
+var nom= $("#nom").add(nom).val();
+var prenom=$("#prenom").add(prenom).val();
+var age=$("#age").add(age).val(); 
+
+
+var pers={'nom':nom, 'prenom': prenom, 'age': age};
+  
+	
+	
+agenda.push(pers);	
+	
+
+console.log(agenda);
+});
+
+
+
+
+
+
+
+
+
+
+
