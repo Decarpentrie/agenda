2b353d40a10d6869e0950e9877f9615e4b3b2f03
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-03-31 14:42:34 +0200

    ajout du bouton supprimer et du tri par ordre alphabetique

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var agenda=[];
 
+
 $("#save").click(function() {
 
 var nom= $("#nom").add(nom).val();
@@ -18,18 +19,28 @@ $("input").val(" ");
 console.log(agenda);
 
 
-$("table").append("<tr><td>"+nom+"</td><td>"+prenom+"</td><td>"+age+"</td></tr>");
+$("table").append("<tr><td>"+nom+"</td><td>"+prenom+"</td><td>"+age+"</td>"+"<td><button class='supr'>x</button></td></tr>");
 
+$("table").tablesorter({sortList: [[ 0 , 0 ], [ 1 , 0 ]]}); 
+
+});
+
+
+$("table").delegate(".supr", "click",  function(){
+
+	$(this).parent().parent().remove();
 
 
 });
-  
 
 
 
 
+//sessionStorage.setItem("");
+//var  = sessionStorage.getItem("");
 
 
-
-
+//.sort(function(a, b) {
+//  return a - b;
+//});
 
